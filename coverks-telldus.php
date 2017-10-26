ffa394b788fdc7eebbc238f98e2d7074e12dc6c2
Morten Hauan <morten@hauan.me>
2017-10-26 19:52:49 +0200

    Test API

--- coverks-telldus.php
+++ coverks-telldus.php
@@ -11,3 +11,34 @@ Version: 0.1a
 Author URI: http://hauan.me
 */
 
+require_once('vendor/autoload.php');
+
+/**
+ * Use this if you're building an application just for you.
+ *
+ * On Telldus, you have the option to create a private token for your user, and don't have to go through the oauth-process
+ * since Telldus generates a token and token_secret for you.
+ *
+ * Go to @see http://api.telldus.com/keys/index and click "Generate a private token for my user only"
+ *
+ * After that you can use the API as follows:
+ */
+
+$api = new Paxx\Telldus\Api(array(
+	'identifier'      => 'FEHUVEW84RAFR5SP22RABURUPHAFRUNU',
+	'secret'          => 'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF',
+	'user_identifier' => '7714006d627959ff8afc1694d6d75d33059dced22',
+	'user_secret'     => 'd6e38b68500972fe8295918c166350d6'
+));
+
+// Get all devices
+$devices = $api->sensor(1310523)->info();
+
+// Holder for the device we want
+$deviceId = null;
+
+echo "<pre>";
+print_r($devices);
+echo "</pre>";
+
+die();
