9292e1ff64b664f68423e60e8db506a4b3ba0342
Morten Hauan <morten@hauan.me>
2017-10-26 19:10:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# coverks-telldus
---
+++ coverks-telldus.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Coverks_Telldus
+ * @version 0.1a
+ */
+/*
+Plugin Name: Coverks Telldus
+Description: Integration with Telldus Live API for WordPress
+Author: Morten Hauan
+Version: 0.1a
+Author URI: http://hauan.me
+*/
+
