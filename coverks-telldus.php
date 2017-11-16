28c54340fee97633edaf5c36cf8faab845d87fed
Morten Hauan <morten@hauan.me>
2017-11-16 23:04:02 +0100

    Get sensors

--- coverks-telldus.php
+++ coverks-telldus.php
@@ -40,3 +40,18 @@ function coverks_telldus_api_light_off( $id ) {
 	return $api->device($id)->turnOff();
 
 }
+
+function coverks_telldus_get_outside_info() {
+	require_once( 'vendor/autoload.php' );
+
+
+	$api = new Paxx\Telldus\Api( array(
+		'identifier'      => 'FEHUVEW84RAFR5SP22RABURUPHAFRUNU',
+		'secret'          => 'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF',
+		'user_identifier' => '7714006d627959ff8afc1694d6d75d33059dced22',
+		'user_secret'     => 'd6e38b68500972fe8295918c166350d6'
+	) );
+
+	return $api->sensors();
+
+}
