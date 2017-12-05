3220145aa0d08e8bf466a2fa5043151e7e0d9d3f
Morten Hauan <morten@hauan.me>
2017-12-05 10:44:01 +0100

    add inside temperature

--- coverks-telldus.php
+++ coverks-telldus.php
@@ -61,3 +61,24 @@ function coverks_telldus_get_outside_info() {
 	return $return_valie;
 
 }
+
+function coverks_telldus_get_inside_info() {
+	require_once( 'vendor/autoload.php' );
+
+
+	$api = new Paxx\Telldus\Api( array(
+		'identifier'      => 'FEHUVEW84RAFR5SP22RABURUPHAFRUNU',
+		'secret'          => 'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF',
+		'user_identifier' => '7714006d627959ff8afc1694d6d75d33059dced22',
+		'user_secret'     => 'd6e38b68500972fe8295918c166350d6'
+	) );
+
+	$sensor_info = $api->sensor(1512099955)->info();
+
+	$return_valie = [
+		"temperature" => $sensor_info['data'][0]['value'],
+		"humidity" => $sensor_info['data'][1]['value'],
+	];
+	return $return_valie;
+
+}
